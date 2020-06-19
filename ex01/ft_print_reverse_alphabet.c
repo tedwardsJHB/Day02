126c41607d5caf65b39ff4ddff5e538ec1ca76f5
Tashreeq Edwards <tedwards@c4r11s3.wethinkcode.co.za>
2020-06-19 13:04:28 +0200

    done!

--- ex01/ft_print_reverse_alphabet.c
+++ ex01/ft_print_reverse_alphabet.c
@@ -1,1 +1,15 @@
-Helo world
+#include <unistd.h>
+
+	void ft_putchar(char c) {
+		write(1,&c,1);
+} 
+	void ft_print_reverse_alphabet (){
+			
+		char letter;
+		letter = 'z';
+		while(letter>='a'){
+		ft_putchar(letter);
+			letter--;
+		}
+	}
+
