909b51f0d667f9646138117bb0b1165d20fcfd29
Tashreeq Edwards <tedwards@c4r11s3.wethinkcode.co.za>
2020-06-22 11:59:01 +0200

    Im getting the hang of this!

---
+++ ex04/ft_print_comb.c
@@ -0,0 +1,30 @@
+int	ft_putchar(char c);
+void	ft_print_comb(void)
+{
+	int	numbers[3] = {48, 48, 48};
+
+	while(numbers[0] <= 57)
+	{
+		if	((numbers[0] < numbers[1])
+				&& (numbers[1] < numbers[2]))
+		{
+			ft_putchar(numbers[0]);
+			ft_putchar(numbers[1]);
+			ft_putchar(numbers[2]);
+			if (numbers[0] !=55)
+				ft_putchar(',');
+			if (numbers[0] != 55)
+				ft_putchar(' ');
+		}
+		if	(numbers[2]++ >= 57 )
+	}
+	numbers[2] = 48;
+	number[1]++;
+}
+if	(numbers[1] == 58)
+{
+	numbers[1] = 48;
+	numbers[0]++;
+}
+
+
